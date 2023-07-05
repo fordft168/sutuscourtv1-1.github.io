1cef346594bbd52700f40fdfd4bfc52aa60de483
fordft168 <fordzaaagt@gmail.com>
2023-07-05 21:57:40 +0700

    php

---
+++ contact.php
@@ -0,0 +1,378 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Sutus Court</title>
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="author" content="" />
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=|Roboto+Sans:400,700|Playfair+Display:400,700">
+
+    
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/aos.css">
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.timepicker.css">
+    <link rel="stylesheet" href="css/fancybox.min.css">
+    <link rel="stylesheet" href="css/slicknav.css">
+
+    <link rel="stylesheet" href="fonts/ionicons/css/ionicons.min.css">
+    <link rel="stylesheet" href="css/all.min.css">
+
+    <!-- Theme Style -->
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    
+
+    <header class="site-header">
+      <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top">
+          <div class="container">
+            <a class="navbar-brand" href="index.php">
+              <img src="images/logo_sutus.png" alt="Sogo Hotel Logo" class="logo-image">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="rooms.php">Rooms</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="rates.php">Rates</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="facilities.php">Facilities & Service</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="contact.php">Contact</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="location.php">Location</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="reservation.php">Reservation</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </header>
+    
+    <!-- END head -->
+      <!-- Start Slider -->
+      <div class="slider_area">
+        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <section class="site-hero overlay" style="background-image: url(images/hero_1.jpg)" data-stellar-background-ratio="0.5">
+                <div class="container">
+                  <div class="row site-hero-inner justify-content-center align-items-center">
+                    <div class="col-md-10 text-center" data-aos="fade">
+                      <h1 class="heading mb-3">Contact</h1>
+                        <ul class="custom-breadcrumbs mb-4">
+                          <li><a href="index.php">Home</a></li>
+                          <li>&bullet;</li>
+                          <li>Contact</li>
+                        </ul>
+                    </div>
+                  </div>
+                </div>
+                <div class="row site-hero-inner justify-content-center align-items-center">
+                  <div class="col-md-10 text-center">
+                    <a class="mouse smoothscroll" href="#next">
+                      <div class="mouse-icon">
+                        <span class="mouse-wheel"></span>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              </section>
+            </div>
+            <div class="carousel-item">
+              <section class="site-hero overlay" style="background-image: url(images/hero_2.jpg)" data-stellar-background-ratio="0.5">
+                <div class="container">
+                  <div class="row site-hero-inner justify-content-center align-items-center">
+                    <div class="col-md-10 text-center" data-aos="fade">
+                      <h1 class="heading mb-3">Contact</h1>
+                        <ul class="custom-breadcrumbs mb-4">
+                          <li><a href="index.php">Home</a></li>
+                          <li>&bullet;</li>
+                          <li>Contact</li>
+                        </ul>
+                    </div>
+                  </div>
+                  <div class="row site-hero-inner justify-content-center align-items-center">
+                    <div class="col-md-10 text-center">
+                      <a class="mouse smoothscroll" href="#next">
+                        <div class="mouse-icon">
+                          <span class="mouse-wheel"></span>
+                        </div>
+                      </a>
+                    </div>
+                  </div>
+              </section>
+            </div>
+            <!-- Add more carousel items as needed -->
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
+    <!-- END section -->
+
+    <section class="section contact-section" id="next">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-7" data-aos="fade-up" data-aos-delay="100">
+            
+            <form action="#" method="post" class="bg-white p-md-5 p-4 mb-5 border">
+              <div class="row">
+                <div class="col-md-6 form-group">
+                  <label for="name">Name</label>
+                  <input type="text" id="name" class="form-control ">
+                </div>
+                <div class="col-md-6 form-group">
+                  <label for="phone">Phone</label>
+                  <input type="text" id="phone" class="form-control ">
+                </div>
+              </div>
+          
+              <div class="row">
+                <div class="col-md-12 form-group">
+                  <label for="email">Email</label>
+                  <input type="email" id="email" class="form-control ">
+                </div>
+              </div>
+              <div class="row mb-4">
+                <div class="col-md-12 form-group">
+                  <label for="message">Write Message</label>
+                  <textarea name="message" id="message" class="form-control " cols="30" rows="8"></textarea>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-6 form-group">
+                  <input type="submit" value="Send Message" class="btn btn-primary text-white font-weight-bold">
+                </div>
+              </div>
+            </form>
+
+          </div>
+          <div class="col-md-5" data-aos="fade-up" data-aos-delay="200">
+            <div class="row">
+              <div class="col-md-10 ml-auto contact-info">
+                <p><span class="d-block">Address:</span> <span> 420/100 Moo 9, Soi Buakhaw, Nongprue, Banglamung, Chon Buri 20150, Thailand.</span></p>
+                <p><span class="d-block">Phone:</span> <span> +66(0) 38428632-3</span></p>
+                <p><span class="d-block">Email:</span> <span> sutuscourt@hotmail.com</span></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="section testimonial-section">
+      <div class="container">
+        <div class="row justify-content-center text-center mb-5">
+          <div class="col-md-7">
+            <h2 class="heading" data-aos="fade-up">People Says</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="js-carousel-2 owl-carousel mb-5" data-aos="fade-up" data-aos-delay="200">
+            
+            <div class="testimonial text-center slider-item">
+              <div class="author-image mb-3">
+                <img src="images/person_1.jpg" alt="Image placeholder" class="rounded-circle mx-auto">
+              </div>
+              <blockquote>
+
+                <p>&ldquo;Good value hotel.Great location.Good Staff.Nice Pool.&rdquo;</p>
+              </blockquote>
+              <p><em>&mdash; Mark</em></p>
+            </div> 
+
+            <div class="testimonial text-center slider-item">
+              <div class="author-image mb-3">
+                <img src="images/person_2.jpg" alt="Image placeholder" class="rounded-circle mx-auto">
+              </div>
+              <blockquote>
+                <p>&ldquo;Room - 10/10
+                  Hotel Area - 10/10
+                  Locality 100/100 Soi Bukhao 🔝
+                  Staffs 100/100
+                  Bathroom is Good.
+                  Balcony is Awesome.
+                  Room Chairs are so Good.
+                  House Keeping So Good & Respectful(usually they are not in Pattaya).
+                  AC is Very Good.
+                  Swimming Pool is Lovely.
+                  Spacious Room.
+                  Wifi is Good.
+                  &rdquo;</p>
+              </blockquote>
+              <p><em>&mdash; Prasanna</em></p>
+            </div>
+
+            <div class="testimonial text-center slider-item">
+              <div class="author-image mb-3">
+                <img src="images/person_3.jpg" alt="Image placeholder" class="rounded-circle mx-auto">
+              </div>
+              <blockquote>
+                <p>&ldquo;location is super,2 pools are nice,aircon strong,wifi strong,everything around you never need transportation for nightlife and daily needs,beach taxi 10 or 20 baht.&rdquo;</p>
+              </blockquote>
+              <p><em>&mdash; Frank</em></p>
+            </div>
+
+
+            <div class="testimonial text-center slider-item">
+              <div class="author-image mb-3">
+                <img src="images/person_4.jpg" alt="Image placeholder" class="rounded-circle mx-auto">
+              </div>
+              <blockquote>
+
+                <p>&ldquo;Went out of their way to find a spot to park my car.Check in always easy and no deposits.Staff are smiling and happy to get some tourist traffic.&rdquo;</p>
+              </blockquote>
+              <p><em>&mdash; Redman</em></p>
+            </div> 
+
+            <div class="testimonial text-center slider-item">
+              <div class="author-image mb-3">
+                <img src="images/person_5.jpg" alt="Image placeholder" class="rounded-circle mx-auto">
+              </div>
+              <blockquote>
+                <p>&ldquo;You are close to everything: food, buses, money changers, rentals, bars, shops.... everything you could ask for.....
+
+                  The staff was friendly and helpful.
+                  
+                  The pool was clean.&rdquo;</p>
+              </blockquote>
+              <p><em>&mdash; James</em></p>
+            </div>
+
+            <div class="testimonial text-center slider-item">
+              <div class="author-image mb-3">
+                <img src="images/person_6.jpg" alt="Image placeholder" class="rounded-circle mx-auto">
+              </div>
+              <blockquote>
+
+                <p>&ldquo;Great value,reception staff very helpful and friendly. excellent location,will book again soon.&rdquo;</p>
+              </blockquote>
+              <p><em>&mdash; Robert</em></p>
+            </div>
+
+          </div>
+            <!-- END slider -->
+        </div>
+
+      </div>
+    </section>
+
+    
+    
+    <section class="section bg-image overlay" style="background-image: url('images/hero_4.jpg');">
+        <div class="container" >
+          <div class="row align-items-center">
+            <div class="col-12 col-md-6 text-center mb-4 mb-md-0 text-md-left" data-aos="fade-up">
+              <h2 class="text-white font-weight-bold">A Best Place To Stay. Reserve Now!</h2>
+            </div>
+            <div class="col-12 col-md-6 text-center text-md-right" data-aos="fade-up" data-aos-delay="200">
+              <a href="reservation.php" class="btn btn-outline-white-primary py-3 text-white px-5">Reserve Now</a>
+            </div>
+          </div>
+        </div>
+      </section>
+
+    <footer class="section footer-section">
+        <div class="container">
+          <div class="row mb-4">
+            <div class="col-md-3 mb-5">
+              <ul class="list-unstyled link">
+                <li><a href="home.php"><img src="images/logo_sutus2.png" alt="Sutus Court Logo"></a></li>
+                <li><a href="rooms.php">Rooms</a></li>
+                <li><a href="rates.php">Rates</a></li>
+              </ul>
+            </div>
+            <div class="col-md-3 mb-5">
+              <ul class="list-unstyled link">
+                <li><a href="facilities.php">Facilities &amp; Service</a></li>
+                <li><a href="contact.php">Contact us</a></li>
+                <li><a href="location.php">Location</a></li>
+                <li><a href="reservation.php">Reservation</a></li>
+              </ul>
+            </div>
+            <div class="col-md-3 mb-5 pr-md-5 contact-info">
+              <p>
+                <span class="d-block"><span class="ion-ios-location h5 mr-3 text-primary"></span>Address:</span>
+                <span>420/100 Moo 9, Soi Buakhaw,</span><br>
+                <span>Nongprue, Banglamung,</span><br>
+                <span>Chon Buri 20150, Thailand.</span>
+              </p>
+              <p>
+                <span class="d-block"><span class="ion-ios-telephone h5 mr-3 text-primary"></span>Phone:</span>
+                <span><a href="tel:+66(0) 38428632-3">+66(0) 38428632-3</a></span>
+              </p>
+              <p>
+                <span class="d-block"><span class="ion-ios-email h5 mr-3 text-primary"></span>Email:</span>
+                <span><a href="mailto:sutuscourt@hotmail.com">sutuscourt@hotmail.com</a></span>
+              </p>
+            </div>
+            <div class="col-md-3 mb-5">
+              <p>Sign up for our newsletter</p>
+              <form action="#" class="footer-newsletter">
+                <div class="form-group">
+                  <input type="email" class="form-control" placeholder="Email...">
+                  <button type="submit" class="btn"><span class="fas fa-paper-plane"></span></button>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div class="row pt-5">
+            <p class="col-md-6 text-left">
+              &copy; <script>document.write(new Date().getFullYear());</script> All rights reserved Sutus Court
+            </p>
+            <p class="col-md-6 text-right social">
+              <a href="#"><span class="fab fa-facebook"></span></a>
+              <a href="#"><span class="fab fa-twitter"></span></a>
+              <a href="https://th.tripadvisor.com/Hotel_Review-g293919-d2283542-Reviews-Sutus_Court-Pattaya_Chonburi_Province.php" target="_blank"><img src="images/tripadvisor-icon.png" alt="TripAdvisor" class="small-icon"></a>
+            </p>
+          </div>
+        </div>
+      </footer>
+    
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/jquery-migrate-3.0.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.stellar.min.js"></script>
+    <script src="js/jquery.fancybox.min.js"></script>
+    <script src="js/fontawesome.min.js"></script>
+    
+    <script src="js/aos.js"></script>
+    
+    <script src="js/bootstrap-datepicker.js"></script> 
+    <script src="js/jquery.timepicker.min.js"></script> 
+
+    
+
+    <script src="js/main.js"></script>
+  </body>
+</html>
